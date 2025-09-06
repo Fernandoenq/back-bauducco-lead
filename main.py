4b494be9786055f2fe8faff59fdb98e2199774f5
unknown <juliacristinalopes2607@gmail.com>
2025-09-05 22:47:16 -0300

    aaaaaa

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db.session import Base, engine
 from routes.user import router as users_router
 
 app = FastAPI(
     title="Cadastro de Pessoas",
     version="1.0.0",
+)
+
+# Configuração de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # libera para qualquer origem
+    allow_credentials=True,
+    allow_methods=["*"],  # libera todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # libera todos os headers
 )
 
 # cria as tabelas ao subir a aplicação
