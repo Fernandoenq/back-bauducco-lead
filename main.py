da88445c2a9fee74db160922e2533750a11f9ff4
Fernando Almeida da Silva Filho <fernandoasfilho74@gmail.com>
2025-10-08 13:04:28 -0300

    ,

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = FastAPI(title="Cadastro de Pessoas", version="1.0.0")
 ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3334",
-    "http://162.120.185.211:3334"
-    "http://54.207.194.150:3334"# Front AWS
-    "http://54.207.194.150:3334/"
-    "http://172.31.5.144:3334"
-    "http://ec2-54-207-194-150.sa-east-1.compute.amazonaws.com:3334"
+    "http://162.120.185.211:3334",
+    "http://54.207.194.150:3334",# Front AWS
+    "http://54.207.194.150:3334/",
+    "http://172.31.5.144:3334",
+    "http://ec2-54-207-194-150.sa-east-1.compute.amazonaws.com:3334",
     "http://127.0.0.1:3000",
     "http://192.168.0.73:3000",   # front Next na tua máquina (ajuste IP/porta)
     "http://192.168.0.73:3334",        # se usar porta 80
